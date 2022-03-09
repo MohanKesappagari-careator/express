a2af18265d7706491a72c22ce9f1c073aa582444
MohanKesappagari-careator <mohan.k@careator.com>
2022-03-09 19:07:15 +0530

    New

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 5001;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const NAMES = ["rev", "sudha", "shake", "mo"];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/name", (req, res) => {
+  res.send(NAMES);
+});
+
+app.post("/name", async (req, res) => {
+  const { name } = req.body;
+  NAMES.push(name);
+  res.send(NAMES);
+});
+
+app.put("/name/:na", (req, res) => {
+  const { name } = req.body;
+  const { na } = req.params;
+
+  let INDEX = NAMES.indexOf(na);
+  NAMES[INDEX] = name;
+  res.send(NAMES);
+});
+
+app.delete("/name/:na", (req, res) => {
+  const { na } = req.params;
+  let INDEX = NAMES.indexOf(na);
+
+  NAMES.splice(INDEX, 1);
+  res.send(NAMES);
+});
+app.listen(PORT, () => console.log(PORT));
